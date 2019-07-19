64988046c0238636740d87d5a89319660a81144b
Natdenice <clad1987@hotmail.com>
2019-07-19 19:34:43 +0200

    Updates

---
+++ precache-manifest.4d54cfbc9031116c97e3d9a8ee0b9ff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9216c1c05ed280006c01dbc7c56b96d9",
+    "url": "/todoApp_React/index.html"
+  },
+  {
+    "revision": "755fb536a0ce7c81beeb",
+    "url": "/todoApp_React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/todoApp_React/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "755fb536a0ce7c81beeb",
+    "url": "/todoApp_React/static/js/main.4627150b.chunk.js"
+  },
+  {
+    "revision": "ca5c41d43fc70ff079e8",
+    "url": "/todoApp_React/static/js/runtime~main.2c68e58d.js"
+  }
+]);
